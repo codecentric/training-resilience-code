cd41379904c563db9e9bbc366f08afa893a5a2fb
Benjamin Wilms <benjamin.wilms@codecentric.de>
2016-11-29 16:39:50 +0100

    Step 1: Good Morning Command

--- hystrix-code-examples/src/main/java/com/codecentric/resilience/hystrix/commands/CommandGoodMorning.java
+++ hystrix-code-examples/src/main/java/com/codecentric/resilience/hystrix/commands/CommandGoodMorning.java
@@ -1,7 +1,18 @@
 package com.codecentric.resilience.hystrix.commands;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
 /**
  * @author Benjamin Wilms (xd98870)
  */
-public class CommandGoodMorning {
+public class CommandGoodMorning extends HystrixCommand<String> {
+
+    protected CommandGoodMorning(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey(null));
+    }
+
+    protected String run() throws Exception {
+        return null;
+    }
 }
--- hystrix-code-examples/src/test/java/com/codecentric/resilience/hystrix/commands/CommandGoodMorningTest.java
+++ hystrix-code-examples/src/test/java/com/codecentric/resilience/hystrix/commands/CommandGoodMorningTest.java
@@ -1,8 +1,20 @@
-import static org.junit.Assert.*;
+package com.codecentric.resilience.hystrix.commands;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Benjamin Wilms (xd98870)
  */
 public class CommandGoodMorningTest {
 
+    @Test
+    public void goodMorningCommandTest() throws Exception {
+
+        String result = new CommandGoodMorning(null).execute();
+
+        assertThat(result, is("Hallo, xxDeinNamexx!"));
+    }
 }
