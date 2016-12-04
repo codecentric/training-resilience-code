e0d3c859c8de9393b9767f72e7031174477fc18b
Benjamin Wilms <benjamin.wilms@codecentric.de>
2016-12-04 13:54:05 +0100

    Step 7: Cold observable

--- hystrix-code-examples/src/test/java/com/codecentric/resilience/hystrix/commands/ReactiveExecutionCommand.java
+++ hystrix-code-examples/src/test/java/com/codecentric/resilience/hystrix/commands/ReactiveExecutionCommand.java
@@ -2,11 +2,8 @@ package com.codecentric.resilience.hystrix.commands;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
-
 import rx.Observable;
-import rx.Observer;
 
 /**
  * @author Benjamin Wilms (xd98870)
@@ -25,13 +22,16 @@ public class ReactiveExecutionCommand {
         String single = observe.toBlocking().single();
 
         assertThat(single, is("Hallo, " + james + "!"));
-        
+
     }
 
     @Test
     public void reactiveCommand_ColdObservable() throws Exception {
 
+        final String james = "James";
+        final String result = "Hallo, " + james + "!";
+
+        CommandGoodMorning commandGoodMorning = new CommandGoodMorning(james);
 
     }
 }
-
